e8632b6f895104fe0bb89e5449c0e851279d67b7
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-22 21:41:24 +0900

    update: topページ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'picture_blogs#index'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :picture_blogs do
