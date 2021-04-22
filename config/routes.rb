111fc765e7d1b73596b3654732a60b8a23103cfb
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-22 18:13:54 +0900

    update: session/picture_blog_controller作成、user/picture_blog_model作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'picture_blogs/new'
+  get 'picture_blogs/index'
+  get 'picture_blogs/show'
+  get 'picture_blogs/edit'
+  get 'picture_blogs/confirm'
   root 'users#new'
   resources :users
 end
