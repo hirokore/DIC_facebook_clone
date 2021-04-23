8aec7e19caebd28187b39b3395fb898579fdbae7
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-23 11:53:41 +0900

    update: アカウント登録機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      # ログインもする機能
+      redirect_to new_user_path, notice: "ユーザが作成されました！"
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def params_user
+    params.require(:user).permit(:name, :email, :password)
+  end
+  
 end
