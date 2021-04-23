ca9549f82d86cb7c66a1770a1443d4a765d01241
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-24 00:45:45 +0900

    update: CRUD機能作成

--- app/controllers/picture_blogs_controller.rb
+++ app/controllers/picture_blogs_controller.rb
@@ -1,8 +1,26 @@
 class PictureBlogsController < ApplicationController
+  before_action :set_picture_blog, only: [ :show, :edit, :update, :destroy ]
+
   def new
+    if params[:back]
+      @picture_blog = PictureBlog.new(picture_blog_params)
+    else
+      @picture_blog = PictureBlog.new
+    end
+  end
+
+  def create
+    @picture_blog = PictureBlog.new(picture_blog_params)
+    @picture_blog.user_id = current_user.id
+    if @picture_blog.save
+      redirect_to picture_blogs_path, notice: "投稿できました！"
+    else
+      render :new
+    end
   end
 
   def index
+    @picture_blog = PictureBlog.all
   end
 
   def show
@@ -12,5 +30,21 @@ class PictureBlogsController < ApplicationController
   end
 
   def confirm
+    @picture_blog = PictureBlog.new(picture_blog_params)
+  end
+
+  def destroy
+    @picture_blog.destroy
+    redirect_to picture_blogs_path, notice: "削除できました！"
+  end
+
+  private
+
+  def picture_blog_params
+    params.require(:picture_blog).permit(:title, :content, :image, :image_cache)
+  end
+
+  def set_picture_blog
+    @picture_blog = PictureBlog.find(params[:id])
   end
 end
