28c902cbe2b1b3528586fd8ef8f940e5f059231f
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-26 02:13:44 +0900

    update: タイムゾーン修正

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,11 @@ module FbAppClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
-
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
