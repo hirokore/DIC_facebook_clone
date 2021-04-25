6011695bc4a73954694cfd9ceff3dd31f14fde0e
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-25 10:31:22 +0900

    update: picture_blogにvalidate機能を追加

--- app/models/picture_blog.rb
+++ app/models/picture_blog.rb
@@ -1,4 +1,8 @@
 class PictureBlog < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+
+  validates :title, presence: true, length: {maximum: 50}
+  validates :content, presence: true, length: {maximum: 1000}
+  
 end
